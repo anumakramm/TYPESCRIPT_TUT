d527ab8ff12377ec3c50266ad239ae42934f04cf
Anum <anumakram3@gmail.com>
2025-06-04 16:00:43 -0400

    data types, function, parameters, return type, interface, custom objects, type union, intersection

---
+++ tutorial.js
@@ -0,0 +1,19 @@
+"use strict";
+// let id: number = 5;
+// let company: string = "Acme Corp";
+// let isPublihshed: boolean = true;
+const User = {
+    id: 2,
+    name: "Pedro",
+    // age: 22,
+    greet(message) {
+        console.log(message);
+    },
+};
+// User.age = "22";
+User.greet("Hello");
+// if(!User.age){
+//     console.log("No Age Of the User");
+// }else{
+// console.log(User.age);
+// }
